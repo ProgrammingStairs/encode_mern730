0152d7a14191b601396cb0f4bb69fe1db8d69d9f
gauravKothari <kotharigaurav6@gmail.com>
2026-01-09 20:26:54 +0530

    mail

--- Express/10 Mail_sending_image_uploading/myApp/controller/userController.js
+++ Express/10 Mail_sending_image_uploading/myApp/controller/userController.js
@@ -2,17 +2,26 @@ import userSchema from "../schema/userSchema.js";
 import mailer from "./mailer.js";
 export const saveController = async(request,response)=>{
     try{
-        mailer.mailer(request.body.email,async (value)=>{
-            if(value){
-                var result = await userSchema.create(request.body);
-                console.log("User Added Successfully : ",result);
-                response.render("login.ejs",{message:"Mail Sent Please Verify"});      
+        var obj = await userSchema.findOne({email:request.body.email});
+        console.log("received obj : ",obj);
+        
+        if(obj && obj.emailVerify==true){
+            response.render("login.ejs",{message:"You are already Verified User | Please Login"});
+        }else if(obj && obj.emailVerify==false){
+                response.render("login.ejs",{message:"This Email Id Already registered | Either verify or try with another email id"});      
+        }else{
+            mailer.mailer(request.body.email,async (value)=>{
+                if(value){
+                    var result = await userSchema.create(request.body);
+                    console.log("User Added Successfully : ",result);
+                    response.render("login.ejs",{message:"Mail Sent Please Verify"});      
 
-            }else{
-                console.log("Something went wrong in sending mail");
-                response.render("registration.ejs",{message:"Something went wrong in sending mail | please try again"});
-            }
-        });
+                }else{
+                    console.log("Something went wrong in sending mail");
+                    response.render("registration.ejs",{message:"Something went wrong in sending mail | please try again"});
+                }
+            });
+        }
 
     }catch(error){
         console.log("Error in saveController : ",error);
@@ -23,9 +32,44 @@ export const saveController = async(request,response)=>{
 export const verifyEmailController = async(request,response)=>{
     try{
         var email = request.body.email;
-        console.log("received email : ",email);
+        // console.log("received email : ",email);
+        var updateStatus = {
+            $set : {
+                emailVerify:true
+            }
+        }
+        var result = await userSchema.updateOne({email},updateStatus);
+        console.log("Result : ",result);
+        if(result.modifiedCount==1){
+            response.render("login.ejs",{message:"Email Verified Successfully | Now you can Login"});
+        }else{
+            response.render("login.ejs",{message:"Email Already Verified"});
+        }
         
     }catch(error){
         console.log("Errro while verify email : ",error);        
+    }
+}
+
+export const loginController = async(request,response)=>{
+    try{
+        var email = request.body.email;
+        var password = request.body.password;
+        var status = {
+            $and : [
+                {email:email},
+                {password:password}
+            ]
+        }
+        var result = await userSchema.findOne(status);
+        console.log("Result : ",result);
+        // if(result.modifiedCount==1){
+        //     response.render("login.ejs",{message:"Email Verified Successfully | Now you can Login"});
+        // }else{
+        //     response.render("login.ejs",{message:"Email Already Verified"});
+        // }
+        
+    }catch(error){
+        console.log("Errro while login : ",error);        
     }
 }
--- Express/10 Mail_sending_image_uploading/myApp/router/userRouter.js
+++ Express/10 Mail_sending_image_uploading/myApp/router/userRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { saveController,verifyEmailController } from '../controller/userController.js';
+import { saveController,verifyEmailController,loginController } from '../controller/userController.js';
 
 var userRouter = express.Router();
 
 userRouter.post("/addUser",saveController);
 userRouter.post("/verifyEmail",verifyEmailController);
+userRouter.post("/login",loginController);
 
 export default userRouter;
